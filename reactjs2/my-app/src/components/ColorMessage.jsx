52dcdd04df35822c6dcdba612e4360263017585c
uijong0729 <krocha0729@gmail.com>
2022-05-07 18:34:11 +0900

    소소한 주석 추가

--- reactjs2/my-app/src/components/ColorMessage.jsx
+++ reactjs2/my-app/src/components/ColorMessage.jsx
@@ -1,4 +1,6 @@
 
+// export const ColorMessage 도 가능
+// 해당 함수만 export 해서 사용하려면 app.jsx에서 import {ColorMessage} from "./components/ColorMessage" 를 맨 위에 추가한다.
 const ColorMessage = (props) => {
     // 분할대입으로 점 접근으로 작성하는 타이핑의 피로를 줄임
     const {color, children} = props;
