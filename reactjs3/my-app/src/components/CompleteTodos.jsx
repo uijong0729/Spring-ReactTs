25b4ad20d56e4488fa915be666864be22e3cd5fe
uijong0729 <krocha0729@gmail.com>
2022-05-24 21:12:02 +0900

    css in js test

--- reactjs3/my-app/src/components/CompleteTodos.jsx
+++ reactjs3/my-app/src/components/CompleteTodos.jsx
@@ -1,10 +1,20 @@
 import React from "react";
 
+// Css in js는 바람직한지 어떤지 아직 논의중인 사항
+const style = {
+  backgroundColor: '#c1ffff',
+  width: '400px',
+  height: '30px',
+  borderRadius: '8px',
+  padding: '8px',
+  margin: '8px'
+}
+
 export const CompleteTodos = (props) => {
     const {arrTodo, funcPermanentlyDelete} = props;
 
     return(
-      <div className='complete-area default-space'>
+      <div style={style}>
         <p className='title'>완료TODO</p>
         <ul>
           { arrTodo.map((todo, i) => {
