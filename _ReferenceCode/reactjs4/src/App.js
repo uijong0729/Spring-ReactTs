870f418acb1a748bc117c06bd54b5fbd0dad600d
uijong0729 <krocha0729@gmail.com>
2022-05-28 20:22:33 +0900

    ready

--- _ReferenceCode/reactjs4/src/App.js
+++ _ReferenceCode/reactjs4/src/App.js
@@ -1,4 +1,6 @@
 import {useState} from "react";
+import {ChildArea} from "./ChildArea"
+
 
 function App() {
   // useState로 상태가 변할때마다 화면에 적용
@@ -8,10 +10,26 @@ function App() {
     setCount(count + 1);
   }
 
+  const [text, setText] = useState('');
+  const [open, setOpen] = useState(false);
+
+  const onChangeText = (e) => setText(e.target.value);
+  const onClickOpen = () => setOpen(!open);
+
   return (
     <div className="App">
       <p>{count}</p>
       <button onClick={onClickCountUp}>count up</button>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <input value={text} onChange={onChangeText}/>
+      <button onClick={onClickOpen}>표시</button>
+      <ChildArea open={open}></ChildArea>
     </div>
   );
 }
