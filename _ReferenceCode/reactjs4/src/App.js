389509fde3a5b45f0189618b204c556dfe8a51cb
uijong0729 <krocha0729@gmail.com>
2022-05-29 10:18:33 +0900

    memo, useMemo, useCallback

--- _ReferenceCode/reactjs4/src/App.js
+++ _ReferenceCode/reactjs4/src/App.js
@@ -1,4 +1,4 @@
-import {useState} from "react";
+import {useState, useCallback, useMemo} from "react";
 import {ChildArea} from "./ChildArea"
 
 
@@ -15,21 +15,31 @@ function App() {
 
   const onChangeText = (e) => setText(e.target.value);
   const onClickOpen = () => setOpen(!open);
+  
+  // 함수를 props로 넘겨주면 매번 함수가 생성되어 렌더링이 다시 발생하므로 useCallback을 사용한다.
+  // useCallback : setOpen값이 바뀌었을때 재생성, 이외에는 새로 만들지 않는다.
+  const onClickClose = useCallback(() => setOpen(false), [setOpen]);
+
+  // useMemo : 변수 자체의 memo화
+  // 변수 자체는 무겁지 않으므로 매번 재렌더링되어도 문제없지만
+  // 변수대입식 처리 자체가 무거운경우 useMemo로 변수가 변경되었을때만 화면에 렌더링을 수행하도록 할 수 있다.
+  const temp = useMemo(() => 1+3, []);
+  console.log(temp);
 
   return (
     <div className="App">
       <p>{count}</p>
       <button onClick={onClickCountUp}>count up</button>
-      <br></br>
-      <br></br>
-      <br></br>
+      <br/>
+      <br/>
+      <br/>
       <br/>
       <br/>
       <br/>
       <br/>
       <input value={text} onChange={onChangeText}/>
       <button onClick={onClickOpen}>표시</button>
-      <ChildArea open={open}></ChildArea>
+      <ChildArea open={open} onClickClose={onClickClose}></ChildArea>
     </div>
   );
 }
--- _ReferenceCode/reactjs4/src/ChildArea.jsx
+++ _ReferenceCode/reactjs4/src/ChildArea.jsx
@@ -1,19 +1,31 @@
+import { memo } from "react";
+
 const style = {
     width: "100%",
     height: "200px",
     backgroundColor: "khaki"
 }
-
-export const ChildArea = (props) => {
-    const {open} = props;
+// 렌더링의 조건
+// - state, props가 갱신된 컴포넌트는 다시 렌더링된다.
+// - 다시 렌더링된 컴포넌트의 자식요소도 다시 렌더링된다.
+// - memo로 감싸면 props가 갱신되지 않는한 재렌더링을 수행하지 않는다.
+// 너무 작은 요소는 memo로 감쌀 필요는 없지만 대부분의 컴포넌트는 memo로 감싸는 것이 좋다.
+export const ChildArea = memo((props) => {
+    const {open, onClickClose} = props;
+    // 렌더링 부하 확인용 2000개의 배열
+    const data = [...Array(2000).keys()];
+    data.forEach(()=>{
+      console.log("ChildArea가 렌더링 되었다.")
+    })
 
     return (
         <>
         {open ? (
           <div style={style}>
             <p>child</p>
+            <button onClick={onClickClose}>닫는다</button>
           </div>
         ) : null}
         </>
     )
-}
+});
