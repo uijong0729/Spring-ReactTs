62c7a389aa4721d12b2e9ee7c51be653328fbf85
uijong0729 <krocha0729@gmail.com>
2022-06-01 23:49:01 +0900

    todo

---
+++ spring/demo/src/main/java/com/example/demo/Bean/Todo.java
@@ -0,0 +1,11 @@
+package com.example.demo.Bean;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Todo {
+    Long oodoid;
+    String todoText;
+}
--- spring/demo/src/main/java/com/example/demo/controller/TestController.java
+++ spring/demo/src/main/java/com/example/demo/controller/TestController.java
@@ -1,10 +1,10 @@
 package com.example.demo.controller;
 
+import com.example.demo.Bean.Todo;
+
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController("/test")
@@ -13,5 +13,10 @@ public class TestController {
     @GetMapping("/ok")
     public ResponseEntity<String> test() {
         return ResponseEntity.ok("ok");
+    }
+
+    @GetMapping("/putTodo")
+    public ResponseEntity<Todo> putTodo(@RequestBody Todo todo) {
+        return ResponseEntity.ok(todo);
     }
 }
