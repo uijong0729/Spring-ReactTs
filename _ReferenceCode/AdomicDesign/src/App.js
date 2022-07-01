8c7186bc97d7e8aeac4245ce09226ef17fe79c44
uijong0729 <krocha0729@gmail.com>
2022-07-01 21:25:51 +0900

    아토믹 디자인으로 작성된 페이지

--- _ReferenceCode/AdomicDesign/src/App.js
+++ _ReferenceCode/AdomicDesign/src/App.js
@@ -1,29 +1,9 @@
-import { BrowserRouter } from 'react-router-dom';
 import './App.css';
-import { SearchInput } from './components/atoms/molecules/SearchInput';
-import { UserCard } from './components/atoms/organisms/user/UserCard';
-import { DefaultLayout } from './components/atoms/page_template/DefaultLayout';
-import { HeaderOnly } from './components/atoms/page_template/HeaderOnly';
-
-const user = {
-  name: "John hoge",
-  img: "https://source.unsplash.com/NE0XGVKTmcA",
-  email: "asdfg12345@example.com",
-  phone: "000-1111-2222",
-  department: {
-    name: "구매부",
-    position: "대리"
-  }
-}
+import { MyRouter } from './page-router/MyRouter';
 
 function App() {
   return (
-    <BrowserRouter>
-      <DefaultLayout>
-        <SearchInput>검색</SearchInput>
-        <UserCard user={user}/>
-      </DefaultLayout>
-    </BrowserRouter>
+    <MyRouter/>
   );
 }
 
---
+++ _ReferenceCode/AdomicDesign/src/components/atoms/page_pages/Users.jsx
@@ -0,0 +1,54 @@
+import styled from "styled-components"
+import { HeaderOnly } from './../page_template/HeaderOnly';
+import { SearchInput } from './../molecules/SearchInput';
+import { UserCard } from "../organisms/user/UserCard";
+
+const users = [...Array(10).keys()].map((val) => {
+    return {
+        id: val,
+        name: `John hoge ${val}`,
+        img: "https://source.unsplash.com/NE0XGVKTmcA",
+        email: "asdfg12345@example.com",
+        phone: "000-1111-2222",
+        department: {
+          name: "구매부",
+          position: "대리"
+        }
+    };
+});
+
+console.log(users)
+
+export const Users = () => {
+    return(
+        <HeaderOnly>
+            <SContainer>
+                <h2>User List</h2>
+                <SearchInput />
+
+                <SUserArea>{
+                    users.map((user) => (
+                        <UserCard key={user.id} user={user}/>
+                    ))
+                }</SUserArea>
+
+            </SContainer>
+        </HeaderOnly>
+    )
+}
+
+const SUserArea = styled.div`
+    padding-top: 40px;
+    width: 100%;
+    display: grid;
+    grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
+    grid-gap: 20px;
+`
+
+const SContainer = styled.div`
+    text-align: center;
+    display: flex;
+    flex-direction: column;
+    align-tiems: center;
+    padding: 24px;
+`
