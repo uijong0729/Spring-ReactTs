83126d06c2cd1b14df7a060a3942b4a1f677b183
uijong0729 <krocha0729@gmail.com>
2022-07-17 19:45:33 +0900

    타입스크립트 + 리액트 준비

--- _ReferenceCode/Typescript_Sample/my-app/src/prac/Prac1.tsx
+++ _ReferenceCode/Typescript_Sample/my-app/src/prac/Prac1.tsx
@@ -1,3 +1,6 @@
+import axios from 'axios';
+
+// https://jsonplaceholder.typicode.com/users
 export const Prac1 = () => {
     // 인수형 : number
     // 반환형 : string
@@ -12,10 +15,21 @@ export const Prac1 = () => {
         alert(원)
     };
 
+    const onClickFetchData = () => {
+        axios.get("https://jsonplaceholder.typicode.com/todos").then((res)=>{
+            // console.log(res.data);
+        })
+    }
+
     return(
         <div>
-            <p>연습1</p>
-            <button onClick={onClickPrac1}>실행</button>
+            <div>
+                <p>연습1</p>
+                <button onClick={onClickPrac1}>실행</button>
+            </div>
+            <div>
+                <button onClick={onClickFetchData}>데이터 취득</button>
+            </div>
         </div>
     )
 }
