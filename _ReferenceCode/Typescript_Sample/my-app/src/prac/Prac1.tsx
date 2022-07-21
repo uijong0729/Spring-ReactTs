052510ee8bc9ecf744fb602ba6a4a8fe2f7e425f
uijong0729 <krocha0729@gmail.com>
2022-07-21 22:27:37 +0900

    axios취득 데이터를 타입을 지정해서 받아오기

--- _ReferenceCode/Typescript_Sample/my-app/src/prac/Prac1.tsx
+++ _ReferenceCode/Typescript_Sample/my-app/src/prac/Prac1.tsx
@@ -2,35 +2,28 @@ import axios from 'axios';
 import { useState } from 'react';
 import { Todo } from '../todo/Todo';
 
+// 타입은 첫문자 대문자
+//  변수명 : 타입
+type TodoType = {
+    userId :number;
+    id :number;
+    title :string;
+    completed :boolean;
+};
+
 // https://jsonplaceholder.typicode.com/users
 export const Prac1 = () => {
-    const [todos, setTodos] = useState<any>([]);
-    
-    // 인수형 : number
-    // 반환형 : string
-    const calcTotalFee = (num :number) :string => {
-        const total :number = num * 1.1;
-        console.log(total);
-        return total + "원";
-    }
-
-    const onClickPrac1 = () => {
-        const 원 :string = calcTotalFee(1000);
-        alert(원)
-    };
+    const [todos, setTodos] = useState<Array<TodoType>>([]);
 
     const onClickFetchData = () => {
-        axios.get("https://jsonplaceholder.typicode.com/todos").then((res)=>{
+        // 타입을 지정해서 가져오기
+        axios.get<Array<TodoType>>("https://jsonplaceholder.typicode.com/todos").then((res)=>{
             setTodos(res.data);
         })
     }
 
     return(
         <div>
-            <div>
-                <p>연습1</p>
-                <button onClick={onClickPrac1}>실행</button>
-            </div>
             <div>
                 <button onClick={onClickFetchData}>데이터 취득</button>
                 {
