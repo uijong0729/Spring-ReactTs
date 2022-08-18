06e8fc2a52f95c2837888da90a5db71a2a021e2d
uijong0729 <krocha0729@gmail.com>
2022-08-18 19:39:01 +0900

    useState 미적용버그 수정

--- spring/demo/react_src/src/hooks/useTodos.tsx
+++ spring/demo/react_src/src/hooks/useTodos.tsx
@@ -5,15 +5,40 @@ import { useState } from "react";
 import { TodoEntity } from "./TodoEntity"
 
 // 모든 유저 일람을 취득하는 커스텀 훅
-export const useAllTodos = () => {
+export const useTodos = () => {
     const [todoEntities, setTodoEntities] = useState<Array<TodoEntity>>([]);
+        
+    // curl -X POST http://localhost:8080/putTodo -H "Content-Type: application/json" -d "{ \"text\": \"johnny\" }"
+    const addTodo = () => {
+      axios.post("http://localhost:8080/putTodo", {
+          text: 'john'
+      })
+      // 취득 결과 확인
+      .then((response) => {
+        console.log(response);
+        console.log(response.data);
+        const res :TodoEntity = {
+          id: response.data.id,
+          text: response.data.text,
+          status: response.data.status
+        };
+        
+        todoEntities.push(res);
+        setTodoEntities([...todoEntities]);
+      })
+      // 에러가 발생할 경우
+      .catch((error)=>{
+        console.log(error);
+      })
+    }
+
 
     const getAllTodos = () => {
         axios.get<Array<TodoEntity>>("http://localhost:8080/getTodo")
         .then((response) => {
           console.log(response);
           console.log(response.data);
-          const res :Array<TodoEntity> = response.data.map((todo) => ({
+          const res :Array<TodoEntity> = response.data.map((todo :TodoEntity) => ({
             id: todo.id,
             text: todo.text,
             status: todo.status
@@ -28,6 +53,6 @@ export const useAllTodos = () => {
 
     // 다른 곳에서 사용할 수 있도록 반환
     return {
-      getAllTodos, todoEntities
+      getAllTodos, todoEntities, addTodo
     };
 };
--- spring/demo/react_src/src/pages/TestPage.tsx
+++ spring/demo/react_src/src/pages/TestPage.tsx
@@ -1,33 +1,19 @@
 import { BlueButton } from '../components/button/BlueButton';
 import axios from 'axios';
-import { useAllTodos } from '../hooks/useTodos';
-
-// curl -X POST http://localhost:8080/putTodo -H "Content-Type: application/json" -d "{ \"text\": \"johnny\" }"
-function addTodo() {
-    axios.post("http://localhost:8080/putTodo", {
-        text: 'john'
-    })
-    // 취득 결과 확인
-    .then((result) => {
-      console.log(result);
-      console.log(result.data);
-    })
-    // 에러가 발생할 경우
-    .catch((error)=>{
-      console.log(error);
-    })
-
-}
+import { useTodos } from '../hooks/useTodos';
 
 export const TestPage = () => {
-    const { todoEntities, getAllTodos} = useAllTodos();
+    const { todoEntities, getAllTodos, addTodo} = useTodos();
+    const onClickLoad = () => getAllTodos();
+    const onAddTodo = () => addTodo();
+
     return (
         <div>
-            <BlueButton onClick={addTodo}>TODO 추가</BlueButton>
+            <BlueButton onClick={onAddTodo}>TODO 추가</BlueButton>
             {todoEntities.map((todos) => (
                 <p>{todos.id} {todos.text} {todos.text}</p>
             ))}
-            <BlueButton onClick={getAllTodos}>TODO 로드</BlueButton>
+            <BlueButton onClick={onClickLoad}>TODO 로드</BlueButton>
         </div>
     )
 }
