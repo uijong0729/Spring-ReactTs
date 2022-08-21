77774dc3a4186abe3d9bfe62837bdf45bc94586e
uijong0729 <krocha0729@gmail.com>
2022-08-21 21:18:30 +0900

    리액트 부트스트랩 적용확인

--- spring/demo/react_src/src/components/button/BaseButton.tsx
+++ spring/demo/react_src/src/components/button/BaseButton.tsx
@@ -1,6 +1,7 @@
+import { FC } from "react"
 import styled from "styled-components"
 
-export const BaseButton = styled.button`
+export const BaseButton :FC = styled.button`
     border-radius: 16px;
     border: 1px solid black;
     padding: 4px 16px;
