02d5537994e22b7b9cfec2be3cb7121d94230498
uijong0729 <krocha0729@gmail.com>
2022-08-30 22:44:17 +0900

    회원가입 구현중

---
+++ spring/demo/react_src/src/components/input/SignUpInput.tsx
@@ -0,0 +1,34 @@
+import { UserEntity } from "../../hooks/api/UserEntity";
+import { useLogin } from "../../hooks/useLogin";
+import { GreenButton } from "../button/GreenButton";
+import { BlueButton } from "../button/BlueButton";
+import { ReactElement } from "react";
+
+export const SignUpInput = () => {
+    const onClickSignUp = () :void  => {
+        const id = document.getElementById('id') as HTMLInputElement;
+        const pass = document.getElementById('pass') as HTMLInputElement;
+
+        const entity :UserEntity = {
+            id: id.value,
+            pass: pass.value
+        }
+    }
+    
+
+    return(
+        <>
+            <dl>
+                <dt>ID</dt>
+                <dd>
+                    <input id="id" type="text" placeholder="아이디를 입력"/>
+                </dd>
+                <dt>PASSWORD</dt>
+                <dd>
+                    <input id="pass" type="password" placeholder="비밀번호를 입력" />
+                </dd>
+            </dl>
+            <BlueButton onClick={onClickSignUp}>만들기</BlueButton>
+        </>
+    )
+}
--- spring/demo/react_src/src/hooks/useLogin.tsx
+++ spring/demo/react_src/src/hooks/useLogin.tsx
@@ -1,5 +1,4 @@
 // 커스텀 훅은 접두어에 use를 붙이는 것이 관례
-
 import axios from "axios";
 import { FC, useState } from "react";
 import { UserEntity } from "./api/UserEntity";
@@ -7,9 +6,11 @@ import { UserEntity } from "./api/UserEntity";
 // 모든 유저 일람을 취득하는 커스텀 훅
 export const useLogin = () => {
     const [userEntity, setUserEntity] = useState<UserEntity>();
+    const env = "http://localhost:8080";
     
+    // 로그인
     const goLogin = (props: UserEntity) => {
-      axios.post<UserEntity>("http://localhost:8080/goLogin", {
+      axios.post<UserEntity>(`${env}/goLogin`, {
           id: props.id,
           pass: props.pass
       }, {
@@ -24,6 +25,25 @@ export const useLogin = () => {
         };
         
         setUserEntity(res);
+      })
+      // 에러가 발생할 경우
+      .catch((error)=>{
+        console.log(error);
+      })
+    }
+
+    // 회원가입
+    const addUser = (param :UserEntity) :void => {
+      axios.post<UserEntity>(`${env}/signup`, {
+          id: param.id,
+          pass: param.pass
+      }, {
+        headers: {
+            'Content-Type': 'application/json'
+        }
+      })
+      .then((response) => {
+        console.log(response);
       })
       // 에러가 발생할 경우
       .catch((error)=>{
