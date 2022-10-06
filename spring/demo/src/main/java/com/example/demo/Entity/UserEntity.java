accf75b4d84b14c8685527770f4628fca8eb5e09
uijong0729 <krocha0729@gmail.com>
2022-10-06 22:55:47 +0900

    로그인 구현 밑준비

--- spring/demo/src/main/java/com/example/demo/Entity/UserEntity.java
+++ spring/demo/src/main/java/com/example/demo/Entity/UserEntity.java
@@ -1,7 +1,10 @@
 package com.example.demo.Entity;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
@@ -9,11 +12,16 @@ import javax.persistence.Id;
 @Entity
 @Getter
 @Setter
+@AllArgsConstructor
 public class UserEntity {
     @Id @GeneratedValue
     private long uid;
+    @Column(name = "id")
     private String id;
+    @Column(name = "pass")
     private String pass;
+    @Column(name = "groupId")
+    private String groupId;
 
     public UserEntity() {
 
@@ -22,6 +30,7 @@ public class UserEntity {
     public UserEntity(String id, String pass) {
         this.id = id;
         this.pass = pass;
+        this.groupId = ""; // 개인용 ToDo
     }
 
     @Override
