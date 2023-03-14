e1c1534f41202464a3c90941acffdda671779c6b
krocha0729@gmail.com <krocha0729@gmail.com>
2023-03-14 23:56:44 +0900

    angular start

--- spring/demo/angular_src/todo-app/src/app/app.component.ts
+++ spring/demo/angular_src/todo-app/src/app/app.component.ts
@@ -2,8 +2,12 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+    <div>test</div>
+  `,
+  styles: [`
+    
+  `]
 })
 export class AppComponent {
   title = 'todo-app';
